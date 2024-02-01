ad3bfa3f2500feffe455eb53e60b515b52b92e85
Swayam Shrestha <swayams24@gmail.com>
2024-02-01 21:19:57 +1100

    Hover works even when grid is reset

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
+// Initialize grid
 const container = document.querySelector("#container");
 let gridSize = 16;
 let grid = gridSize * gridSize;
 
+resetGrid();
 createGrid(grid);
+hover();
 
-const hover = document.querySelectorAll(".grid");
-hover.forEach(item => {
-    item.addEventListener('mouseover', (e) => {
-        e.target.classList.add("hover");
-    })
-});
+// Set hover state
+function hover(){
+    const hover = document.querySelectorAll(".grid");
+    hover.forEach(item => {
+        item.addEventListener('mouseover', (e) => {
+            e.target.classList.add("hover");
+        })
+    });
+}
 
+//Change Grid Size
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', () => {
+    gridSize = prompt("Enter Grid Size");
+    if (gridSize <= 100){
+        resetGrid();
+        grid = gridSize * gridSize;
+        createGrid(grid);
+        hover();
+    } else {
+        alert("Maximum Limit Exceeded!")
+    }
+})
+
+const reset = document.querySelector(".reset");
+reset.addEventListener('click', () => {
+    resetGrid();
+    createGrid(grid);
+    hover();
+
+    // hover state doesnst work add that shit
+    // add repeat block on css and shit so its gridSize by gridSize depending on the input
+})
+
+//Create Grid func
 function createGrid(grid){
     for (let i = 1; i <= grid; i++){
         const grid = document.createElement("div");
         grid.classList.add("grid");
         container.appendChild(grid);
     }
+}
+
+//Reset Grid func
+function resetGrid(){
+    const list = document.querySelectorAll(".grid");
+    list.forEach(item => {
+        item.remove();
+    })
 }
